bfa67737fecc2a927c3de7e1a74bdf5ef8763bb4
Daniel <danydu1299@gmail.com>
2022-11-03 19:36:24 -0500

    Setup static and media files

--- biblio_app/settings/development.py
+++ biblio_app/settings/development.py
@@ -16,23 +16,23 @@ SITE_NAME = "BiBlioApp"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": env("POSTGRES_ENGINE"),
-#         "NAME": env("POSTGRES_DB"),
-#         "USER": env("POSTGRES_USER"),
-#         "PASSWORD": env("POSTGRES_PASSWORD"),
-#         "HOST": env("PG_HOST"),
-#         "PORT": env("PG_PORT"),
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # GRAPH_MODELS = {
 #     "all_applications": True,
--- biblio_app/urls.py
+++ biblio_app/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 
 urlpatterns = [
@@ -10,7 +13,7 @@ urlpatterns = [
     # path('api/', include("apps.users.urls")),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # path('accounts/', include('django.contrib.auth.urls'))
-    ]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
 
